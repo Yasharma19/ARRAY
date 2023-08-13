c9e07494d9742f4140414a75d5d590921aaeaea4
Yash Sharma <yash304sharma@gmail.com>
2023-08-13 23:05:22 +0530

    Update BestTimeToBuyAndSellStock.java

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -1,15 +1,18 @@
 class Solution {
-    //run loop from 1 to end and check the minimum element from last array from 0 to that element.
-
     public int maxProfit(int[] prices) {
-        int minn=prices[0];
-        int profit=0;
-        for(int i=1;i<prices.length;i++){
-            int cost=prices[i]-minn;
-            profit=Math.max(profit,cost);
-            minn=Math.min(minn,prices[i]);
-        }
+        int min_val=Integer.MAX_VALUE;
+        int max_profit=0;
+        for(int i=0;i<prices.length;i++){
 
-        return profit;
+            //updating min value
+            if(prices[i]<min_val){
+                min_val=prices[i];
+            }           
+            //getting profit when selling today, and check whether getting maximum profit or not
+            else if(prices[i]-min_val>max_profit){
+                max_profit=prices[i]-min_val;
+            }
+        }
+        return max_profit;
     }
 }
