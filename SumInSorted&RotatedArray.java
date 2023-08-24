5869fbb9ae10c3bd3f97a1691fd5aa369f2189e5
Yash Sharma <yash304sharma@gmail.com>
2023-08-24 15:53:12 +0530

    Update SumInSorted&RotatedArray.java

--- SumInSorted&RotatedArray.java
+++ SumInSorted&RotatedArray.java
@@ -1,5 +1,41 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static boolean pairInSortedRotated(int[] arr,int N,int X){
+        Arrays.sort(arr);
+        int i=0;
+        int j=N-1;
+        while(i<j){
+            if(arr[i]+arr[j]==X){
+                return true;
+            }
+            else if(arr[i]+arr[j]<X){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = { 11, 15, 6, 8, 9, 10 };
+        int X = 144;
+        int N = arr.length;
+ 
+        if (pairInSortedRotated(arr, N, X))
+            System.out.print("true");
+        else
+            System.out.print("false");
+    }
+}
+
+
+
+...................................................................................................................................................................
+
+
+//Another SOLUTION //
 
 class HelloWorld {
     
