16e4f9225a95d5559c3b3365df37a8193a746ad1
Yash Sharma <yash304sharma@gmail.com>
2023-08-25 14:38:51 +0530

    Update MinimumInRotatedSortedArray.java

--- MinimumInRotatedSortedArray.java
+++ MinimumInRotatedSortedArray.java
@@ -1,3 +1,26 @@
+//BEST SOLUTION USING PIVOT ELEMENT 
+class Solution{
+    public int findMin(int[] nums) {
+        int n=nums.length;
+        int i;          //Finding pivot element(Highest element);
+        
+        if(nums[0]<nums[n-1] || n==1){  //if length==0 or whether an array is in sorted manner
+            return nums[0];
+        }
+        for(i=0;i<n-1;i++){
+            if(nums[i]>nums[i+1]){
+                break;
+            }
+            //Here i is the index of Largest element and just after it there is a smallest element so we return element at i+1 index
+        }
+        return nums[i+1];   
+    }
+}
+
+
+.............................................................................................................................
+//ANOTHER SOLUTION WHICH IS LESS EFFICIENT
+
 class Solution {
     public int findMin(int[] nums) {
         int n=nums.length;
